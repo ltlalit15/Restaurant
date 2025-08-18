0307ebf8ce2cfc68867cc060949715f39e537c91
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-08-18 19:05:08 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,10 +5,11 @@ dotenv.config();
 
 // Create connection pool
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: "mainline.proxy.rlwy.net",   // Railway host
+  user: "root",                      // Railway user
+  password: "KAErjQuChBKKFuEOJVoEJtMmGizbsjFY",  // Railway password
+  database: "railway",               // Railway DB name
+  port: 42087,                       // Railway port
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
